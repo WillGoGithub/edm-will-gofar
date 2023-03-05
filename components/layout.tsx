97605c6325d8adeeffaa95e2499cee1a567c70af
+W <willohyayo@gmail.com>
2023-03-05 15:40:40 +0800

    feat: transform to ts

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,13 @@ import Link from 'next/link';
 const name = 'Will Lai';
 export const siteTitle = ' IT Consulting Service';
 
-export default function Layout({ children, home }) {
+export default function Layout({
+    children,
+    home,
+}: {
+    children: React.ReactNode;
+    home?: boolean;
+}) {
     return (
         <div className={styles.container}>
             <Head>
